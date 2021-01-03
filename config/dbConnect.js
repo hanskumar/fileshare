4881c58515bac3989ebef10550b3e80f9d4d5b83
hanskumar <hanskumar58@gmail.com>
2021-01-03 23:38:13 +0530

    remove some code

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -13,14 +13,4 @@ module.exports = (req, res) => {
     }).catch(err=>{
         console.log("Connection::"+ err);
     });
-
-
-    /* mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log('Database connected 🥳🥳🥳🥳');
-    }).catch(err => {
-        console.log(err);
-    }); */
 }
